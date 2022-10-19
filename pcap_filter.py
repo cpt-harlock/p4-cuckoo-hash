56153584ba3832983dd7e97614e9fd07820a9cb0
harlock <monterubbiano@di.uniroma1.it>
2022-10-19 14:02:14 +0200

    updated pcap_filter.py

--- pcap_filter.py
+++ pcap_filter.py
@@ -1,16 +1,23 @@
 from scapy.all import *
 import os
+import sys
 
-capture = input("Enter file path of pcap file: " )
+#capture = input("Enter file path of pcap file: " )
+
+capture = sys.argv[1]
 pcap = rdpcap(capture)
+out = sys.argv[2]
 
 s = set()
-os.remove('filtered.pcap')
+try:
+    os.remove('filtered.pcap')
+except :
+    print("no file to remove")
 
 def write(pkt):
-    wrpcap('filtered.pcap', pkt, append=True, linktype=101 )  #appends packet to output file
+    wrpcap(out, pkt, append=True, linktype=101 )  #appends packet to output file
 
-for pkt in pcap:
+for i,pkt in enumerate(pcap):
     ip_hdr = pkt.getlayer(IP)
     if ip_hdr.src not in s:  #checks for UDP layer and sport 137
         write(pkt)  #sends the packet to be written if it meets criteria
